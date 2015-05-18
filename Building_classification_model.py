80d3256f19a2ed84e5e8aef8a3b945a2541c418b
Z.-L. Deng <dawnmy@163.com>
2015-05-18 16:43:10 +0200

    Create Building_classification_model.py
    This script is used to build the classification model based on selected biomarkers by using Support Vector Machine with linear model.

---
+++ Building_classification_model.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Author: Z.-L. Deng
+
+# This script is used to build the classification model based on selected biomarkers
+# by using Support Vector Machine with linear model.
+
+
+"""
+Building the classification model using SVM method.
+"""
+
+import numpy as np
+from numpy import arange
+from sklearn import preprocessing
+from sklearn.svm import SVC
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report, matthews_corrcoef
+
+
+print(__doc__)
+
+# For loading the data from tab delimited file
+def read_data_table(inf):
+    inputfile = inf
+
+    datamatrix = np.array([map(float,line.split()[1:]) for line in open(inputfile).readlines()])
+
+    # Scaling the value in to range [0,1]
+    min_max_scaler = preprocessing.MinMaxScaler()
+    data_scaled = min_max_scaler.fit_transform(datamatrix)
+
+    label = np.array(map(int,[line.split()[0] for line in open(inputfile).readlines()]))
+
+    dataform = {"data":data_scaled,"label":label}
+
+    return(dataform)
+
+if __name__=="__main__":
+
+    # The selected 4 best biomarker candidates
+    markers = [45,63,68,77]
+
+    # Loading the traning data set  from file
+    top100 = read_data_table("top100.txt")
+    # Loading the external test data set from file
+    jorth100 = read_data_table("jo_bwa_19seed_top100.txt")
+
+    X,y = top100["data"][:,markers],top100["label"]
+    X_train, X_test, y_train, y_test = X,X,y,y
+    Jx_test,Jy_test = jorth100["data"][:,markers],jorth100["label"]
+
+    # Grid search for best parameters
+    tuned_parameters = [
+                    {'kernel': ['linear'], 'C': [2**i for i in arange(-5,  7, 0.2)]}]
+
+    # Precision and fi score are used as the benchmark
+
+    scores = ['precision','f1']
+
+    for score in scores:
+        print("\n")
+        print("#-----------------------------------------------------------------------------------------#")
+        print("# Tuning hyper-parameters for %s                                                  #" % score)
+        print("#-----------------------------------------------------------------------------------------#")
+
+        clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=3, scoring=score)
+        
+        clf.fit(X_train, y_train)
+
+        print("Best parameters set found on data set:")
+        print("------------------------------------------------------------------------------------------")
+        print(clf.best_estimator_)
+        print("\n")
+        print("------------------------------------------------------------------------------------------")
+
+
+        print("Detailed classification report:")
+        print("------------------------------------------------------------------------------------------")
+        print("The model is trained on the full data set.")
+        print("The scores are computed on the full evaluation set.")
+        print("------------------------------------------------------------------------------------------")
+
+        #--prediction
+        y_true, y_pred = y_test, clf.predict(X_test)
+
+        Jy_true,Jy_pred = Jy_test,clf.predict(Jx_test)
+        
+        print("Training data set-------------------------------------------------------------------------")
+
+        print(classification_report(y_true, y_pred))
+        print("MCC: ")
+        print(matthews_corrcoef(y_true,y_pred))
+
+
+        print  y_true,"\n",y_pred
+        print("#####--------------------------------------------------------------------------------#####")
+        print("\n")
+
+        print("External test data set--------------------------------------------------------------------")
+        print(classification_report(Jy_true, Jy_pred))
+        print("MCC: ")
+        print(matthews_corrcoef(Jy_true,Jy_pred))
+        print  Jy_true,"\n",Jy_pred
